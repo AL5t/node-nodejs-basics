2fe40af05331b4bf828b23c2e7c35d1db3be9ab2
AL5t <oalsim5@gmail.com>
2025-10-26 23:58:00 +0300

    feat: add solutions for wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os';
+
 const performCalculations = async () => {
-  // Write your code here
+  const numOfCPUs = os.cpus().length;
+  const results = [];
+  const workers = [];
+
+  for (let i = 0; i < numOfCPUs; i++) {
+    const worker = new Worker('./worker.js');
+    const numberToSend = 10 + i;
+
+    worker.postMessage(numberToSend);
+
+    worker.on('message', (result) => {
+      results[i] = { status: 'resolved', data: result };
+      worker.terminate();
+      if (results.length === numOfCPUs && !results.includes(undefined)) {
+        console.log(results);
+      }
+    });
+
+    worker.on('error', (error) => {
+      results[i] = { status: 'error', data: null };
+      worker.terminate();
+      if (results.length === numOfCPUs && !results.includes(undefined)) {
+        console.log(results);
+      }
+    });
+
+    workers.push(worker);
+  }
 };
 
 await performCalculations();
