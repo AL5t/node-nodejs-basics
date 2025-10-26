1b29e01bb7ae2b56f4dba42f9bd4a44ad0e72e00
AL5t <oalsim5@gmail.com>
2025-10-27 01:30:33 +0300

    feat: add solution for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { spawn } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const childProcess = spawn('node', ['./files/script.js', ...args]);
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.pipe(process.stdout);
+
+  childProcess.stderr.pipe(process.stderr);
+
+  process.on('exit', () => {
+    childProcess.kill();
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg-1', 'arg-2', 'arg-2', 'arg-4']);
